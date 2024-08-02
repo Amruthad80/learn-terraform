e7af215f4f7e7cf36b5071c03af0d6a3125a7366
amrutha-dve <amrutha.irrinki95@gmail.com>
2024-08-02 14:20:36 +0530

    terraform code

--- roboshop-v1/ec2.tf
+++ roboshop-v1/ec2.tf
@@ -26,6 +26,16 @@ resource "aws_instance" "mongo" {
   }
 }
 
+
+resource "aws_route53_record" "mongo" {
+  zone_id = "Z0920686IGN3402SVP12"
+  name    = "mongo.dev.devopsb72.online"
+  type    = "A"
+  ttl     = 15
+  records = [aws_instance.mongo.private_ip]
+}
+
+
 resource "aws_instance" "catalogue" {
   ami           = "ami-041e2ea9402c46c32"
   instance_type = "t3.small"
@@ -34,4 +44,12 @@ resource "aws_instance" "catalogue" {
   tags = {
     Name = "catalogue.dev"
   }
+}
+
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z0920686IGN3402SVP12"
+  name    = "catalogue.dev.devopsb72.online"
+  type    = "A"
+  ttl     = 15
+  records = [aws_instance.catalogue.private_ip]
 }
