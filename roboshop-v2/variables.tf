31add00c181cc2dcdd4902676a7b45b04e06174f
amrutha-dve <amrutha.irrinki95@gmail.com>
2024-08-02 22:42:14 +0530

    terraform code

---
+++ roboshop-v2/variables.tf
@@ -0,0 +1,19 @@
+variable "ami" {
+    defualt = "ami-041e2ea9402c46c32"
+}
+
+variable "instance_type" {
+    default = "t3.small"
+}
+
+variable "security_group_ids" {
+    default = ["sg-0ec8efc096a610505"]
+}
+
+variable "zone_id" {
+    default = "Z0920686IGN3402SVP12"
+}
+
+variable "domain_name" {
+    default = "devopsb72.online"
+ }
