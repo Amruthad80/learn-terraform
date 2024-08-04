e170acced3573fbcf18b5f7fc056a36c67998cb8
amrutha-dve <amrutha.irrinki95@gmail.com>
2024-08-04 15:42:20 +0530

    terraform code

--- roboshop-v5/main.tf
+++ roboshop-v5/main.tf
@@ -28,7 +28,7 @@ resource "null_resource" "ansible" {
             user = "ec2_user"
             password = "DevOps321"
             host = aws_instance.instance[each.key].private_ip
-            }
+        }
         inline = [
             "sudo pip-3.11 install ansible",
             "ansible-pull -i localhost, -u https://github.com/Amruthad80/ansible-roboshop main.yml -e env=dev -e role_name=$[each_key]"
